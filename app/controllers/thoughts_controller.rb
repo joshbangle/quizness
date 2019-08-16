0418b67e16bcfc414c846af31edb1d1791c24b8a
Joshua Bangle <joshua.bangle@gmail.com>
2019-08-16 23:16:53 +0000

    About done

--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -1,2 +1,27 @@
 class ThoughtsController < ApplicationController
+
+  def index
+    @thoughts = Thought.all
+
+  end
+
+  def new
+    @thought = Thought.new
+  end
+
+  def create
+    Thought.create(thought_params)
+    redirect_to root_path
+  end
+
+
+
+
+
+private
+
+def thought_params
+  params.require(:thought).permit(:input, :reviewer)
+end
+
 end
